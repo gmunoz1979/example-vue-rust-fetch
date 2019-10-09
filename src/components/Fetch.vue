b45967042450cc5d3dc6d2d69389dc832777a132
Gerard Muñoz <gmunoz1979@gmail.com>
2019-10-08 23:53:22 -0300

    App base

---
+++ src/components/Fetch.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="fetch">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Fetch',
+  props: {
+    msg: String
+  },
+  beforeCreate() {
+    import('../../pkg').then(wasm => {
+      wasm.test("Hello World!!!");
+    });
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
